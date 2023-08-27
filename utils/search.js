153d78e371960e4d340cd936fe11f7f2926b7f0d
陈桢梁 <1003975097@qq.com>
2023-08-27 17:46:38 +0800

    改东西

--- utils/search.js
+++ utils/search.js
@@ -13,6 +13,9 @@ let data = {}
 function flushData() {
     client.get("https://data.rmtc.org.cn/gis/listtype0M.html", {
         headers: {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Connection": "keep-alive",
+            "sec-ch-ua-platform": "Windows",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
         }
     }).then(res => {
